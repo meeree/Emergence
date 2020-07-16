212ae71605e0da032b374b653ddc247789dd4e16
meeree <j.a.hazelden@gmail.com>
2020-07-16 14:08:48 -0600

    Added bird behavior

--- Assets/BirdManager.cs
+++ Assets/BirdManager.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class BirdManager : MonoBehaviour
 {
-    List<GameObject> birds;
+    List<BirdMovement> birds;
+    public float radius = 1f;
 
-    void Awake()
+    void Start ()
     {
-        for (int i = 0; i < this.gameobject.transform.GetChildCount(); i++)
-            birds.Insert(this.gameobject.transform.GetChild(i));
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            birds.Add(child.GetComponent<BirdMovement>());
+        }
     }
 
     void Update()
     {
-        // UPDATE EACH BIRDS NEIGHBORS  
+        // Zero out bird averages 
+        foreach(BirdMovement bird in birds)
+        {
+            bird.neighbor_avg_pos = Vector3.zero;
+            bird.neighbor_avg_angle = 0f;
+        }
+
+        for(int i = 0; i < birds.Count; ++i)
+        {
+            Vector3 p1 = birds[i].gameObject.transform.position;
+            for(int j = i+1; j < birds.Count; ++j)
+            {
+                Vector3 p2 = birds[j].gameObject.transform.position;
+                if((p1 - p2).magnitude <= radius)
+                {
+                    birds[i].neighbor_avg_pos += p2;
+                    birds[j].neighbor_avg_pos += p1;
+
+                    birds[i].neighbor_avg_angle += birds[j].angle;
+                    birds[j].neighbor_avg_angle += birds[i].angle;
+
+                    birds[i].num_neighbors += 1;
+                    birds[j].num_neighbors += 1;
+                }
+            }
+        }
+
+        foreach(BirdMovement bird in birds)
+        {
+            bird.neighbor_avg_pos /= bird.num_neighbors;
+            bird.neighbor_avg_angle /= bird.num_neighbors;
+        }
     }
 }
--- Assets/BirdMovement.cs
+++ Assets/BirdMovement.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class BirdMovement : MonoBehaviour
 {
-    float angle = 0;
-    List<BirdMovement> neighbors;
+    public int num_neighbors = 0;
+    public Vector3 neighbor_avg_pos = Vector3.zero;
+    public float neighbor_avg_angle = 0;
+    public float angle = 0;
+
+    public float speed = 1f;
+
+    Vector3 Alignment()
+    {
+        return new Vector3(Mathf.Cos(neighbor_avg_angle), Mathf.Sin(neighbor_avg_angle), 0);
+    }
+
+    Vector3 Seperation() 
+    {
+        return (transform.position - neighbor_avg_pos).normalized;
+    }
+
+    Vector3 Cohesion()
+    {
+        return (neighbor_avg_pos - transform.position).normalized;
+    }
+
     void Update()
     {
-        dt1 = Seperation();
-        dt2 = Alignment();
-        dt3 = Cohesion();
+        if(num_neighbors == 0)
+            return;
+
+        Vector3 dt1 = Seperation();
+        Vector3 dt2 = Alignment();
+        Vector3 dt3 = Cohesion();
+
+        Vector3 vel = (dt1 + dt2 + dt3);
+        transform.position += Time.deltaTime * speed * vel;
+        angle = Mathf.Atan2(vel.y, vel.x);
     }
 }
